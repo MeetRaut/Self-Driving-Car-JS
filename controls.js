9139ba03e80f165c473e44a9b8f0a891044bb677
Meet Raut <meetraut3004@gmail.com>
2024-05-31 20:22:25 +0530

    car functionality added

--- controls.js
+++ controls.js
@@ -27,7 +27,6 @@ class Controls{
                     this.reverse = true;
                     break;
             }
-            console.table(this);
         }
 
         document.onkeyup = (event) =>{
@@ -48,7 +47,6 @@ class Controls{
                     this.reverse = false;
                     break;
             }
-            console.table(this);
         }
     }
 }

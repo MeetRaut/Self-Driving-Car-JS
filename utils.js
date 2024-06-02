bf159db09aac9295fa1b302e679d37ff774be125
Meet Raut <meetraut3004@gmail.com>
2024-06-02 14:56:01 +0530

    added Neural networks

--- utils.js
+++ utils.js
@@ -23,13 +23,13 @@ function getIntersection(A,B,C,D){
 }
 
 function polysIntersect(poly1, poly2){
-    for(let i = 0; i < poly1.length; i++){
-        for(let j = 0; j < poly2.length; j++){
-            const touch = getIntersection(
+    for(let i=0;i<poly1.length;i++){
+        for(let j=0;j<poly2.length;j++){
+            const touch=getIntersection(
                 poly1[i],
                 poly1[(i+1)%poly1.length],
                 poly2[j],
-                poly2[(j+1)%poly2.length],
+                poly2[(j+1)%poly2.length]
             );
             if(touch){
                 return true;
@@ -38,3 +38,11 @@ function polysIntersect(poly1, poly2){
     }
     return false;
 }
+
+function getRGBA(value){
+    const alpha=Math.abs(value);
+    const R=value<0?0:255;
+    const G=R;
+    const B=value>0?0:255;
+    return "rgba("+R+","+G+","+B+","+alpha+")";
+}               
